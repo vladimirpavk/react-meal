2ba1212c5401e659813ba6ac5ea4261d41b31cad
Vladimir Pavković <vladimirpavk@gmail.com>
2020-05-01 16:09:07 +0200

    +

--- Screens/CategoriesScreen.js
+++ Screens/CategoriesScreen.js
@@ -1,28 +1,66 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  FlatList
+} from 'react-native';
 
-const CategoriesScreen = (props) => {
-  console.log(props);
+import { CATEGORIES } from '../data/dummy-data';
 
-  const navigateMeals = ()=>{
-    props.navigation.navigate({routeName: 'CategoryMeals'})
-  }
-
+const renderGridItem = itemData => {
+  console.log(itemData.item);
   return (
-    <View style={styles.container}>
-      <Text>Categories screen</Text>
-      <Button title="Go to meals screen" onPress={navigateMeals} />
+    <View style={[styles.gridItem, {backgroundColor: `${itemData.item.color}`}]}>
+      <Text style={styles.gridItemContent}>{itemData.item.name}</Text>
     </View>
   );
-}
+};
+
+const CategoriesScreen = props => {
+  return (
+    <View style={styles.screen}>
+      <FlatList
+        style={styles.flatStyle}
+        keyExtractor={(item, index) => item.id}
+        data={CATEGORIES}
+        renderItem={renderGridItem}
+        numColumns={2}
+      />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-  container: {
+  screen: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: 'black',
+    backgroundColor: 'white'
   },
+  flatStyle:{
+    flex: 1,
+    width: '100%'
+  },
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 150,
+    padding: 15,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  gridItemContent:{   
+    justifyContent: 'center',
+    width: '100%',
+    color: 'black',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontStyle: 'open-sans-bold'
+  }
 });
 
 export default CategoriesScreen;
---
+++ models/Category.js
@@ -0,0 +1,7 @@
+export class Category{
+    constructor(id, name, color){
+        this.id = id;
+        this.name = name;
+        this.color = color;
+    }
+}
